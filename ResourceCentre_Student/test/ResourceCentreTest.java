0d8148c186ee1b9fb6aaffcb6d753ea5920d8480
19010491 <19010491@DESKTOP-19077C7>
2020-07-22 15:36:59 +0800

    a Change-Id: I0000000000000000000000000000000000000001

--- ResourceCentre_Student/test/ResourceCentreTest.java
+++ ResourceCentre_Student/test/ResourceCentreTest.java
@@ -1,170 +1,3 @@
-<<<<<<< HEAD
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertSame;
-
-import static org.junit.Assert.*;
-
-import java.util.ArrayList;
-
-import org.junit.After;
-import org.junit.Before;
-import org.junit.Test;
-
-public class ResourceCentreTest {
-	private Camcorder cc1;
-	private Camcorder cc2;
-	private Chromebook cb1;
-	private Chromebook cb2;
-
-	private ArrayList<Camcorder> camcorderList;
-	private ArrayList<Chromebook> chromebookList;
-
-	public ResourceCentreTest() {
-		super();
-	}
-
-	@Before
-	public void setUp() throws Exception {
-		// prepare test data
-		cc1 = new Camcorder("CC0011", "Nikon HDSLR", 40);
-		cc2 = new Camcorder("CC0012", "Sony DSC-RX100M7", 20);
-		cb1 = new Chromebook("CB0011", "My Google Chromebook 1st", "Mac OS");
-		cb2 = new Chromebook("CB0012", "SAMSUNG Chromebook 4+", "Win 10");
-
-		camcorderList = new ArrayList<Camcorder>();
-		chromebookList = new ArrayList<Chromebook>();
-	}
-
-	@Test
-	public void addCamcorderTest() {
-		// Item list is not null, so that can add a new item
-		assertNotNull("Test if there is valid Camcorder arraylist to add to", camcorderList);
-
-		// Given an empty list, after adding 1 item, the size of the list is 1
-		ResourceCentre.addCamcorder(camcorderList, cc1);
-		assertEquals("Test if that Camcorder arraylist size is 1?", 1, camcorderList.size());
-
-		// The item just added is as same as the first item of the list
-		assertSame("Test that Camcorder is added same as 1st item of the list?", cc1, camcorderList.get(0));
-
-		// Add another item. test The size of the list is 2?
-		ResourceCentre.addCamcorder(camcorderList, cc2);
-		assertEquals("Test that Camcorder arraylist size is 2?", 2, camcorderList.size());
-	}
-
-	@Test
-	//TBC by Gabriel ;3
-	public void addChromebookTest() {
-		// fail("Not yet implemented");
-		// write your code here
-	}
-
-	@Test
-	public void retrieveAllCamcorderTest() {
-		// Test if Item list is not null but empty, so that can add a new item
-		assertNotNull("Test if there is valid Camcorder arraylist to add to", camcorderList);
-
-		// test if the list of camcorders retrieved from the SourceCentre is empty
-		String allCamcorder = ResourceCentre.retrieveAllCamcorder(camcorderList);
-		String testOutput = "";
-		assertEquals("Check that ViewAllCamcorderlist", testOutput, allCamcorder);
-
-		// Given an empty list, after adding 2 items, test if the size of the list is 2
-		ResourceCentre.addCamcorder(camcorderList, cc1);
-		ResourceCentre.addCamcorder(camcorderList, cc2);
-		assertEquals("Test if that Camcorder arraylist size is 2?", 2, camcorderList.size());
-
-		// test if the expected output string same as the list of camcorders retrieved
-		// from the SourceCentre
-		allCamcorder = ResourceCentre.retrieveAllCamcorder(camcorderList);
-
-		testOutput = String.format("%-10s %-30s %-10s %-10s %-20d\n", "CC0011", "Nikon HDSLR", "Yes", "", 40);
-		testOutput += String.format("%-10s %-30s %-10s %-10s %-20d\n", "CC0012", "Sony DSC-RX100M7", "Yes", "", 20);
-
-		assertEquals("Check that ViewAllCamcorderlist", testOutput, allCamcorder);
-
-	}
-
-	@Test
-	public void retrieveAllChromebookTest() {
-		// fail("Not yet implemented");
-		// write your code here
-	}
-
-	@Test
-	// edited by Devin
-	public void doLoanCamcorderTest() {
-		// fail("Not yet implemented");
-		// write your code here
-
-	}
-
-	@Test
-	//change by christy
-	//this method unit test case designed by me.
-	public void doLoanChromebookTest() {
-		// fail("Not yet implemented");
-		// write your code here
-        //Create UI to obtain the type of items to check out
-		assertNotNull("Create UI to obtain the type of items to check out", camcorderList);
-
-		// Retrieve available items
-		String allCamcorder = ResourceCentre.retrieveAllCamcorder(camcorderList);
-		String testOutput = "";
-		assertEquals("Check that ViewAllCamcorderlist", testOutput, allCamcorder);
-
-		//Create UI to display available items
-		testOutput = String.format("%-10s %-30s %-10s %-10s %-20d\n", "CC0011", "Nikon HDSLR", "Yes", "", 40);
-		testOutput += String.format("%-10s %-30s %-10s %-10s %-20d\n", "CC0012", "Sony DSC-RX100M7", "Yes", "", 20);
-
-		assertEquals("Check that ViewAllCamcorderlist", testOutput, allCamcorder);
-
-
-		//Create UI to obtain the item code to be check out
-		assertEquals("Check that ViewAllCamcorderlistfor the item code", testOutput, allCamcorder);
-		
-		
-		//Update status of item as unavailable
-		allCamcorder = ResourceCentre.retrieveAllCamcorder(camcorderList);
-		
-		
-		//test if the expected output string same as the list of camcorders retrieved
-		// from the SourceCentre
-	}
-
-	// fail("Not yet implemented");
-	
-	
-	
-
-	@Test
-	public void doReturnCamcorderTest() {
-		// fail("Not yet implemented");
-		// write your code here
-
-	}
-
-	@Test
-	public void doReturnChromebookTest() {
-		// fail("Not yet implemented");
-		// write your code here
-	}
-
-	@After
-	public void tearDown() throws Exception {
-		cc1 = null;
-		cc2 = null;
-		cb1 = null;
-		cb2 = null;
-		camcorderList = null;
-		chromebookList = null;
-
-	}
-
-}
-=======
-
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertSame;
@@ -212,16 +45,12 @@ public class ResourceCentreTest {
 		// The item just added is as same as the first item of the list
 		assertSame("Test that Camcorder is added same as 1st item of the list?", cc1, camcorderList.get(0));
 
-		// Add another item. test The size of the list is 2?
+		// Add another item. test The size of the list is 2??
 		ResourceCentre.addCamcorder(camcorderList, cc2);
 		assertEquals("Test that Camcorder arraylist size is 2?", 2, camcorderList.size());
 	}
 
 	@Test
-
-	// change by Gabriel
-	// second change by Gabriel again~ :D
-
 	// TBC by Gabriel ;3
 	public void addChromebookTest() {
 		// fail("Not yet implemented");
@@ -261,8 +90,7 @@ public class ResourceCentreTest {
 	}
 
 	@Test
-	// edited by Devin
-	// fresh reupload
+	// edited by Devin aaaaaaa
 	public void doLoanCamcorderTest() {
 		// fail("Not yet implemented");
 		// write your code here
@@ -273,10 +101,8 @@ public class ResourceCentreTest {
 	// change by christy
 	// this method unit test case designed by me.
 	public void doLoanChromebookTest() {
-
 		// fail("Not yet implemented");
 		// write your code here
-
 		// Create UI to obtain the type of items to check out
 		assertNotNull("Create UI to obtain the type of items to check out", camcorderList);
 
@@ -289,8 +115,6 @@ public class ResourceCentreTest {
 		testOutput = String.format("%-10s %-30s %-10s %-10s %-20d\n", "CC0011", "Nikon HDSLR", "Yes", "", 40);
 		testOutput += String.format("%-10s %-30s %-10s %-10s %-20d\n", "CC0012", "Sony DSC-RX100M7", "Yes", "", 20);
 
-		assertEquals("Check that ViewAllCamcorderlist", testOutput, allCamcorder);
-
 		// Create UI to obtain the item code to be check out
 		assertEquals("Check that ViewAllCamcorderlistfor the item code", testOutput, allCamcorder);
 
@@ -299,7 +123,6 @@ public class ResourceCentreTest {
 
 		// test if the expected output string same as the list of camcorders retrieved
 		// from the SourceCentre
-
 	}
 
 	// fail("Not yet implemented");
@@ -329,4 +152,3 @@ public class ResourceCentreTest {
 	}
 
 }
->>>>>>> branch 'master' of https://github.com/19010491-Gabriel-Ang/ResourceCentre_Student.git
